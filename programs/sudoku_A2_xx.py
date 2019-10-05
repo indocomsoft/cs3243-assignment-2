0ccab2e411c39a58aa8b584916634d9106560c5b
Jolene Tan <zxj.tan@gmail.com>
2019-10-05 17:01:14 +0800

    Initialise row/col/square sets in constructor

--- programs/sudoku_A2_xx.py
+++ programs/sudoku_A2_xx.py
@@ -6,16 +6,29 @@ class Sudoku(object):
         # you may add more attributes if you need
         self.puzzle = puzzle # self.puzzle is a list of lists
         self.ans = copy.deepcopy(puzzle) # self.ans is a list of lists
+        self.rows = [set() for i in xrange(len(puzzle))]
+        self.cols = [set() for i in xrange(len(puzzle))]
+        self.squares = [set() for i in xrange(len(puzzle))]
+        for r in xrange(len(puzzle)):
+            for c in xrange(len(puzzle[r])):
+                number = puzzle[r][c]
+                if number != 0:
+                    self.rows[r].add(number)
+                    self.cols[c].add(number)
+                    self.squares[self.square_num(r, c)].add(number)
 
     def solve(self):
         #TODO: Your code here
 
-        # don't print anything here. just resturn the answer
+        # don't print anything here. just return the answer
         # self.ans is a list of lists
         return self.ans
 
     # you may add more classes/functions if you think is useful
     # However, ensure all the classes/functions are in this file ONLY
+
+    def square_num(self, r, c):
+        return 3 * (r // 3) + c // 3
 
 if __name__ == "__main__":
     # STRICTLY do NOT modify the code in the main function here
