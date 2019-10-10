03afe7aa52a004f786b8c1ee440bbf8f01eabfe8
Jolene Tan <zxj.tan@gmail.com>
2019-10-10 20:13:12 +0800

    I think it works

--- programs/sudoku_A2_xx.py
+++ programs/sudoku_A2_xx.py
@@ -1,9 +1,11 @@
 import sys
 import copy
+from collections import deque
+
+DOMAIN = set(range(1,10))
 
 class Sudoku(object):
     def __init__(self, puzzle):
-        # you may add more attributes if you need
         self.puzzle = puzzle # self.puzzle is a list of lists
         self.ans = copy.deepcopy(puzzle) # self.ans is a list of lists
         self.rows = [set() for i in xrange(len(puzzle))]
@@ -15,20 +17,79 @@ class Sudoku(object):
                 if number != 0:
                     self.rows[r].add(number)
                     self.cols[c].add(number)
-                    self.squares[self.square_num(r, c)].add(number)
+                    self.squares[self.get_square_num(r, c)].add(number)
+
+        self.possible = [[set() for c in xrange(len(puzzle[r]))] for r in xrange(len(puzzle))]
+        self.queue = deque([])
+        # Initialise possibilities
+        for r in xrange(len(puzzle)):
+            for c in xrange(len(puzzle[r])):
+                number = puzzle[r][c]
+                if number != 0:
+                    self.possible[r][c] = {number}
+                else:
+                    self.possible[r][c] = DOMAIN - \
+                        self.rows[r] - \
+                        self.cols[c] - \
+                        self.squares[self.get_square_num(r, c)]
+                    if len(self.possible[r][c]) == 1:
+                        self.queue.append((r, c, self.possible[r][c].copy().pop()))
 
     def solve(self):
-        #TODO: Your code here
+        while self.queue:
+            r, c, v = self.queue.popleft()
+            if self.ans[r][c] == v:
+                continue
+            self.ans[r][c] = v
+            self.rows[r].add(v)
+            self.cols[c].add(v)
+            self.squares[self.get_square_num(r, c)].add(v)
+            self.possible[r][c] = { v }
+            recheck_cells = {(r, i) for i in xrange(len(self.puzzle))} | \
+                {(i, c) for i in xrange(len(self.puzzle))} | \
+                self.same_square(r, c)
+            recheck_cells.remove((r, c))
+            for r, c in recheck_cells:
+                self.possible[r][c].discard(v)
+                if len(self.possible[r][c]) == 0:
+                    return False
+                if len(self.possible[r][c]) == 1 and self.ans[r][c] == 0:
+                    self.queue.append((r, c, self.possible[r][c].copy().pop()))
 
-        # don't print anything here. just return the answer
-        # self.ans is a list of lists
-        return self.ans
+        if self.is_complete():
+            return self.ans
+
+        r, c = self.get_most_constrained()
+        for v in self.possible[r][c]:
+            other = copy.deepcopy(self)
+            other.queue.append((r, c, v))
+            result = other.solve()
+            if result:
+                return result
+        return False
 
     # you may add more classes/functions if you think is useful
     # However, ensure all the classes/functions are in this file ONLY
-
-    def square_num(self, r, c):
+    def get_square_num(self, r, c):
         return 3 * (r // 3) + c // 3
+
+    def same_square(self, r, c):
+        square_num = self.get_square_num(r, c)
+        top_left_r = 0 + square_num // 3 * 3
+        top_left_c = 0 + square_num % 3 * 3
+        return set((top_left_r + r, top_left_c + c) for c in xrange(3) for r in xrange(3))
+
+    def is_complete(self):
+        return all(map(lambda row: 0 not in row, self.ans))
+
+    def get_most_constrained(self):
+        candidate = ((0, 0), 9)
+        for r in xrange(len(self.puzzle)):
+            for c in xrange(len(self.puzzle)):
+                cur_len = len(self.possible[r][c])
+                if 1 < cur_len < candidate[1]:
+                    candidate = ((r, c), cur_len)
+        return candidate[0]
 
 if __name__ == "__main__":
     # STRICTLY do NOT modify the code in the main function here
